296e47e9e6393a659eeb55216e4633d69f59e1cc
nurdamiron <nurdamiron@gmail.com>
2025-05-14 10:16:27 +0500

    scs

--- node-version.js
+++ node-version.js
@@ -7,15 +7,15 @@ console.log('Checking Node.js version compatibility...');
 
 try {
   // Create .nvmrc file to set Node.js version for Vercel
-  fs.writeFileSync(path.join(__dirname, '.nvmrc'), '16.20.0');
-  console.log('Created .nvmrc file with Node.js v16.20.0');
+  fs.writeFileSync(path.join(__dirname, '.nvmrc'), '18.x');
+  console.log('Created .nvmrc file with Node.js v18.x');
 
   // Also set engines in package.json
   const packageJsonPath = path.join(__dirname, 'package.json');
   const packageJson = require(packageJsonPath);
   
   packageJson.engines = {
-    node: "16.x"
+    node: "18.x"
   };
   
   fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
